c9ec915b021636890b584a7e5021b1ac98b749e4
Yegorich75 <yegorich75@gmail.com>
2020-02-25 18:40:27 +0200

    Add Basic Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './Components/Header/Header';
+import './Components/Surf/Surf';
+import './Components/Travel/Travel';
+import './Components/Sleep/Sleep';
+import './Components/Shop/Shop';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          blablabla
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
       </header>
+      <div className="container">
+        <Surf />
+        <Travel />
+        <Sleep />
+        <Shop />
+      </div>
     </div>
   );
 }
