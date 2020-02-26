08341e00dc2a1bb64afda226dfccbdf8cba4e1d8
yegorich75 <yegorich75@gmail.com>
2020-02-26 22:48:08 +0200

    Header Img Bug Fix

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import classes from './Header.module.css';
+import Logo from '../../img/logo.svg';
+import Arrows from '../../img/arrows-down.svg';
 
 const Header = () => {
     return (
         <div>
-            <header className="header">
-                <div className="header-title">Go Surf</div>
-                <div className="header__arrows animated fadeInDown delay-2s slow infinite">
+            <header className={classes.header}>
+                <div className={classes.title}>Go Surf</div>
+                <div className={`${classes.arrows} animated fadeInDown delay-2s slow infinite`}>
                     <a href="#">
-                        <img src="img/arrows-down.svg" alt="Arrow Down" />
+                        <img src={Arrows} alt="Arrow Down" />
                     </a>
                 </div>
-                <div className="header__map"></div>
+                <div className={classes.map}></div>
                 <div className="header-aside">
                     <div className="logo">
                         <a href="#">
-                            <img src="img/logo.svg" alt="Logo" />
+                            <img src={Logo} alt="Logo" />
                         </a>
                     </div>
                 </div>
