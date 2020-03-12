8a45172cc0997a222b8392d97e405170cae45bc2
Yegorich75 <yegorich75@gmail.com>
2020-03-12 14:52:10 +0200

    Slider Problem

--- src/Components/Header/Slider/Slider.jsx
+++ src/Components/Header/Slider/Slider.jsx
@@ -1,100 +1,101 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./Slider.module.css";
 import arrowsRight from "../../../img/arrows-right.svg";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Dotshead from "../SliderDot/Dotshead";
 
-class SimpleSlider extends React.Component {
-  render() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
+export default class AsNavFor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nav1: null,
+      nav2: null
     };
+  }
+
+  componentDidMount() {
+    this.setState({
+      nav1: this.slider1,
+      nav2: this.slider2
+    });
+  }
+
+  render() {
     return (
-      <Slider {...settings}>
-        <div className={styles.slider}>
-          <div className={`${styles.item} header__slider-item`}>
-            <div className={styles.info}>
-              <div className={`${styles.subtitle} headline-left`}>Surf</div>
-              <div className={styles.title}>North Shore</div>
-              <div className={`${styles.subtext} headline-left`}>
-                Condition
+      <div>
+        <Slider
+          asNavFor={this.state.nav2}
+          ref={slider => (this.slider1 = slider)}
+        >
+          <div className={styles.slider}>
+            <div className={`${styles.item} header__slider-item`}>
+              <div className={styles.info}>
+                <div className={`${styles.subtitle} headline-left`}>Surf</div>
+                <div className={styles.title}>North Shore</div>
+                <div className={`${styles.subtext} headline-left`}>
+                  Condition
+                </div>
+                <div className={styles.text}>Radical</div>
+                <a className={styles.link} href="#">
+                  <img src={arrowsRight} alt="#" />
+                </a>
               </div>
-              <div className={styles.text}>Radical</div>
-              <a className={styles.link} href="#">
-                <img src={arrowsRight} alt="#" />
-              </a>
+            </div>
+            <div className={`${styles.item} header__slider-item`}>
+              <div className={styles.info}>
+                <div className={`${styles.subtitle} headline-left`}>Surf</div>
+                <div className={styles.title}>South Shore</div>
+                <div className={`${styles.subtext} headline-left`}>
+                  Condition
+                </div>
+                <div className={styles.text}>Radical</div>
+                <a className={styles.link} href="#">
+                  <img src={arrowsRight} alt="#" />
+                </a>
+              </div>
+            </div>
+            <div className={`${styles.item} header__slider-item`}>
+              <div className={styles.info}>
+                <div className={`${styles.subtitle} headline-left`}>Surf</div>
+                <div className={styles.title}>West Shore</div>
+                <div className={`${styles.subtext} headline-left`}>
+                  Condition
+                </div>
+                <div className={styles.text}>Radical</div>
+                <a className={styles.link} href="#">
+                  <img src={arrowsRight} alt="#" />
+                </a>
+              </div>
+            </div>
+            <div className={`${styles.item} header__slider-item`}>
+              <div className={styles.info}>
+                <div className={`${styles.subtitle} headline-left`}>Surf</div>
+                <div className={styles.title}>East Shore</div>
+                <div className={`${styles.subtext} headline-left`}>
+                  Condition
+                </div>
+                <div className={styles.text}>Radical</div>
+                <a className={styles.link} href="#">
+                  <img src={arrowsRight} alt="#" />
+                </a>
+              </div>
             </div>
           </div>
-          <div className={`${styles.item} header__slider-item`}>
-            <div className={styles.info}>
-              <div className={`${styles.subtitle} headline-left`}>Surf</div>
-              <div className={styles.title}>South Shore</div>
-              <div className={`${styles.subtext} headline-left`}>
-                Condition
-              </div>
-              <div className={styles.text}>Radical</div>
-              <a className={styles.link} href="#">
-                <img src={arrowsRight} alt="#" />
-              </a>
-            </div>
-          </div>
-          <div className={`${styles.item} header__slider-item`}>
-            <div className={styles.info}>
-              <div className={`${styles.subtitle} headline-left`}>Surf</div>
-              <div className={styles.title}>West Shore</div>
-              <div className={`${styles.subtext} headline-left`}>
-                Condition
-              </div>
-              <div className={styles.text}>Radical</div>
-              <a className={styles.link} href="#">
-                <img src={arrowsRight} alt="#" />
-              </a>
-            </div>
-          </div>
-          <div className={`${styles.item} header__slider-item`}>
-            <div className={styles.info}>
-              <div className={`${styles.subtitle} headline-left`}>Surf</div>
-              <div className={styles.title}>East Shore</div>
-              <div className={`${styles.subtext} headline-left`}>
-                Condition
-              </div>
-              <div className={styles.text}>Radical</div>
-              <a className={styles.link} href="#">
-                <img src={arrowsRight} alt="#" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </Slider>
+        </Slider>
+        <Slider
+          asNavFor={this.state.nav1}
+          ref={slider => (this.slider2 = slider)}
+          slidesToShow={3}
+          swipeToSlide={true}
+          focusOnSelect={true}
+        >
+          <Dotshead />
+        </Slider>
+      </div>
     );
   }
 }
 
-// $(function(){
-
-//   $('.slick-slider').slick({
-//       infinite: true,
-//       speed: 1000,
-//       fade: true,
-//       cssEase: 'linear',
-//       prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="Arrow Left"></img>',
-//       nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="Arrow Right"></img>',
-//       asNavFor: '.Dotshead_dotshead__htc9c'
-//   });
-
-//   $('.Dotshead_dotshead__htc9c').slick({
-//       slidesToShow: 4,
-//       slidesToScroll: 4,
-//       asNavFor: '.slick-slider',
-//       dots: true,
-//       // centerMode: true,
-//       focusOnSelect: true
-//   });
-// });
-
-export default SimpleSlider;
+// export default SimpleSlider;
