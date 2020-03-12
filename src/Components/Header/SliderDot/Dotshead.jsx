09d1c2f6c9e0f6539c74aaf7fa094260ab0f6c52
Yegorich75 <yegorich75@gmail.com>
2020-03-12 09:44:01 +0200

    dotshead slider styling start

--- src/Components/Header/SliderDot/Dotshead.jsx
+++ src/Components/Header/SliderDot/Dotshead.jsx
@@ -5,27 +5,27 @@ const Dotshead = () => {
   return (
     <div className={styles.dotshead}>
       <div className="slider-dots__item">
-        <div className="dots-box">
-          <div className="dots-box__number">01</div>
-          <div className="dots-box__name">North Shore</div>
+        <div className={styles.box}>
+          <div className={styles.number}>01</div>
+          <div className={styles.name}>North Shore</div>
         </div>
       </div>
       <div className="slider-dots__item">
-        <div className="dots-box">
-          <div className="dots-box__number">02</div>
-          <div className="dots-box__name">South Shore</div>
+        <div className={styles.box}>
+          <div className={styles.number}>02</div>
+          <div className={styles.name}>South Shore</div>
         </div>
       </div>
       <div className="slider-dots__item">
-        <div className="dots-box">
-          <div className="dots-box__number">03</div>
-          <div className="dots-box__name">West Shore</div>
+        <div className={styles.box}>
+          <div className={styles.number}>03</div>
+          <div className={styles.name}>West Shore</div>
         </div>
       </div>
       <div className="slider-dots__item">
-        <div className="dots-box">
-          <div className="dots-box__number">04</div>
-          <div className="dots-box__name">East Shore</div>
+        <div className={styles.box}>
+          <div className={styles.number}>04</div>
+          <div className={styles.name}>East Shore</div>
         </div>
       </div>
     </div>
